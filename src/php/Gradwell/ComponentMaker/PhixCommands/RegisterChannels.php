7a188b2cfc4ddcceaa26230dda25ac6c58eb56ae
Stuart Herbert <stuart@stuartherbert.com>
2011-03-14 21:12:51 +0000

    Added proper header docblocks to the ComponentMaker section; ported code to work with the extracted libraries

--- src/php/Gradwell/ComponentMaker/PhixCommands/RegisterChannels.php
+++ src/php/Gradwell/ComponentMaker/PhixCommands/RegisterChannels.php
@@ -1,17 +1,59 @@
 <?php
 
+/**
+ * Copyright (c) 2010 Gradwell dot com Ltd.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Gradwell dot com Ltd nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package     Gradwell
+ * @subpackage  ComponentMaker
+ * @author      Stuart Herbert <stuart.herbert@gradwell.com>
+ * @copyright   2010 Gradwell dot com Ltd. www.gradwell.com
+ * @license     http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link        http://gradwell.github.com
+ * @version     @@PACKAGE_VERSION@@
+ */
+
 namespace Gradwell\ComponentMaker\PhixCommands;
 
 use Phix_Project\Phix\CommandsList;
 use Phix_Project\Phix\Context;
 use Phix_Project\PhixExtensions\CommandBase;
 use Phix_Project\PhixExtensions\CommandInterface;
-use Phix_Project\CommandLineLib\DefinedSwitches;
-use Phix_Project\CommandLineLib\DefinedSwitch;
-use Phix_Project\CommandLineLib\CommandLineParser;
-use Phix_Project\ValidationLib\MustBeValidFile;
-use Phix_Project\ValidationLib\MustBeValidPath;
-use Phix_Project\ValidationLib\MustBeWriteable;
+use Gradwell\CommandLineLib\DefinedSwitches;
+use Gradwell\CommandLineLib\DefinedSwitch;
+use Gradwell\CommandLineLib\CommandLineParser;
+use Gradwell\ValidationLib\MustBeValidFile;
+use Gradwell\ValidationLib\MustBeValidPath;
+use Gradwell\ValidationLib\MustBeWriteable;
 
 use Gradwell\ComponentMaker\Entities\LibraryComponentFolder;
 
